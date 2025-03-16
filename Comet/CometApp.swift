382e2fe1033a9f29f7e2fb96c24b396243895ea9
xeone59 <tiankongcaopo@163.com>
2025-03-15 18:54:31 -0700

    Initial commit

---
+++ Comet/CometApp.swift
@@ -0,0 +1,17 @@
+//
+//  CometApp.swift
+//  Comet
+//
+//  Created by 小序 on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CometApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Comet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Comet
+//
+//  Created by 小序 on 3/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
